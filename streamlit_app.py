939a408e88f1907087ba7109561f92fe87680f29
carol <carolina.df@outlook.com>
2025-06-15 19:37:49 -0300

    alteração do model para resposta

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@ import streamlit as st
 from openai import OpenAI
 import os
 import base64
-import requests
 
 # Show title and description.
 st.title("📄 Análise de Vulnerabilidade em Arquitetura de Software")
@@ -13,8 +12,8 @@ st.write(
 # Ask user for their OpenAI API key via `st.text_input`.
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
 # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-
 openai_api_key = os.getenv("OPENAI_API_KEY")
+
 if not openai_api_key:
     openai_api_key = st.text_input("OpenAI API Key", type="password")
     st.info("Insira sua OpenAI API key para continuar", icon="🗝️")
@@ -34,25 +33,31 @@ else:
         messages = [
             {
                 "role": "user",
-                "content": f"""
-                Voce é um agente especialista em arquitetura de sistemas e irá receber uma imagem de arquitetura para análise geral.
-                Seus objetivos são:
-                1. Interpretar a arquitetura e montar uma lista de componentes de cada elemento presente na imagem.
-                Exemplo: AWS - S3, AWS - EC2, AWS - ECR, AWS - Lambda
-                2. Explicar o que cada componente faz. Exemplo: AWS - S3 : Storage em nuvem de arquivos... 
-                3. Explicar o fluxo da aplicação apresentada. Sendo a imagem da architetura: {uploaded_file.name}
-                """
+                "content": [
+                    {"type":"text", "text": f""" Você é um agente especialista em arquitetura de sistemas e irá receber uma imagem de arquitetura para análise geral.
+                     Seus objetivos são:
+                     1. Interpretar a arquitetura e montar uma lista de componentes de cada elemento presente na imagem. Exemplo: AWS - S3, AWS - EC2, AWS - ECR, AWS - Lambda
+                     2. Explicar o que cada componente faz. Exemplo: AWS - S3 : Storage em nuvem de arquivos... 
+                     3. Explicar o fluxo da aplicação apresentada. Sendo a imagem da architetura: {uploaded_file.name}"""
+                    },
+                     {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": f"data:image/jpeg;base64,{document}",
+                            },
+                    },
+                ]
             }
-        ]
-
-        # Generate an answer using the OpenAI API.
-        stream = client.chat.completions.create(
-            model="gpt-4o-mini-2024-07-18",
-            temperature=0.2,
-            max_tokens=3000,
-            messages=messages,
-            stream=True,
-        )
-
-        # Stream the response to the app using `st.write_stream`.
-        st.write_stream(stream)
+        ]        
+        with st.spinner('Analisando arquitetura... Por favor, aguarde.'):
+            stream = client.chat.completions.create(
+                model="o4-mini-2025-04-16",
+                messages=messages,
+            )
+        
+        # TODO: Alterar o prompt para que o retorno seja no formato que a lib de análise da OWASP precisa pra rodar.
+        # TODO: Alterar o retorno para trazer o relatório de vulnerabilidades usando a metodologia STRIDE como base.
+        # TODO: Incluir botão para download do relatório gerado em pdf.
+        
+        st.write(stream.choices[0].message.content)
+        st.success("Análise concluída com sucesso!", icon="✅")
